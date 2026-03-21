673dabaa9513df99a8e5bc826da8b45f753873c6
Augustlyden <august.lyden@outlook.com>
2026-03-21 01:25:27 +0100

    Github pages experiment with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Spells-Archive/', 
+  base: '/Spells-Archive/',
   build: {
-    outDir: 'dist', 
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        spell: resolve(__dirname, 'spell.html'),
+        },
+    },
+  },
 })
